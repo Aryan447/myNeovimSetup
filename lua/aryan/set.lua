9ded72e452d2a7e604d9257f63c389f72c652cb4
aryan447 <89701129+Aryan447@users.noreply.github.com>
2025-06-12 18:27:47 +0530

    initial commit

--- lua/aryan/set.lua
+++ lua/aryan/set.lua
@@ -7,6 +7,7 @@ vim.opt.tabstop = 4
 vim.opt.softtabstop = 4
 vim.opt.shiftwidth = 4
 vim.opt.expandtab = true
+vim.opt.cmdheight = 0
 
 vim.opt.smartindent = true
 
@@ -28,5 +29,5 @@ vim.opt.isfname:append("@-@")
 
 vim.opt.updatetime = 50
 
-vim.opt.colorcolumn = "80"
+vim.opt.colorcolumn = ""
 
