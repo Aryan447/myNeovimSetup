42bc2c878ac3b74e7482a08d5a6740926c50fe63
Aryan447 <singharyan4477@gmail.com>
2026-03-16 11:10:09 +0530

    automated dev commit

--- lua/aryan/lazy/conform.lua
+++ lua/aryan/lazy/conform.lua
@@ -1,6 +1,5 @@
 return {
-    'stevearc/conform.nvim',
-    opts = {},
+    "stevearc/conform.nvim",
     config = function()
         require("conform").setup({
             formatters_by_ft = {
@@ -8,10 +7,24 @@ return {
                 javascript = { "prettier" },
                 typescript = { "prettier" },
                 python = { "black" },
-                ruby= { "rubocop" },
-                go = { "gofmt" }
-            }
-        })
-    end
-}
+                ruby = { "rubocop" },
+                go = { "gofmt" },
+                c = { "clang-format" },
+                cpp = { "clang-format" },
+            },
 
+            formatters = {
+                ["clang-format"] = {
+                    prepend_args = { "-style=file", "-fallback-style=LLVM" },
+                },
+            },
+        })
+
+        vim.keymap.set("n", "<leader>f", function()
+            require("conform").format({
+                bufnr = 0,
+                lsp_fallback = true,
+            })
+        end)
+    end,
+}
